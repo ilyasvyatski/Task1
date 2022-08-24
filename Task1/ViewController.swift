a8927fab38f7521b3e6090f35219f198f9b008a7
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-08-24 17:25:54 +0300

    task 1 color ui label

--- Task1/ViewController.swift
+++ Task1/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var colorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //colorButton.backgroundColor = .clear
+        colorButton.layer.cornerRadius = 8
+        colorButton.layer.borderWidth = 1
+        colorButton.layer.borderColor = UIColor.blue.cgColor
     }
 
-
+    @IBAction func colorButton(_ sender: UIButton) {
+        colorLabel.textColor = getRandomColor()
+    }
+    
+    func getRandomColor() -> UIColor {
+            return UIColor(red: .random(in: 0...1),
+                           green: .random(in: 0...1),
+                           blue: .random(in: 0...1),
+                           alpha: 1.0)
+    }
+    
 }
+
 
